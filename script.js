5c785a2fa1b98c0e5cbae521606b4e03a2f3d78b
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-09-17 15:52:50 -0700

    added to dos, added scaffolding in html - remove later

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log(moment().format(hA));
+
+//TODO: create one row with time, text area, and button using - jq
+
+    //design layout in html first 
+//     <form class="row hour time-block">
+                
+//     <article class="col-md-2">
+//         9am
+//     </article>
+
+//     <textarea class="col-md-8">
+        
+//     </textarea>
+
+    
+//     <button class="saveBtn col-md-2">
+
+//     </button>
+    
+//      </form>
+//TODO: find a way to append these in the order 
+    // find a way to populate with appropriate information
+        //add time recognition using moment.js - look at classes in html 
+
+//TODO: display current day at top of page use moment.js a p tag already exists
+
+//TODO:create array to hold the hours 9am -5pm
+
+//TODO:using a for loop
+
+//TODO:loop over my array of hours and create form for row, create article, create textarea, create button,
+
+//TODO:give them content, append them to the row, and then append the row to the page
+         
+//TODO:how can i compare the hours of my rows to the current time (use moment.js to get current time) use an array? 
+        // round hours? - or dont use minutes in programing - look under format in moment.js
+
+//TODO: give classes that elements need past, future, present  - ise if statments comparing current time to row time 
+//if===, class, if < , if >
+
+//TODO: click eventlistener for save button, im using a form element - stop propagation!!
+
+//TODO: grab the value of the text area, save to a variable, text needs to be row specific - 9 data variables 
+
+//TODO: save to local storage - localStorage.setItem - 
+
+//TODO: on refresh, the data persists - retrieve data on page load - localStorage.getItem - show data back to where it came from
+        // how can i know what text from local storage goes to what area) - try to make the key inherent with the click area .this? replace button with radio?
+
